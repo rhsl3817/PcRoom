a1d3956420d504228d3ec3ace78a71d59b2cf195
rhsl3817 <rhsl3817@gmail.com>
2022-02-23 09:40:11 +0900

    init

---
+++ src/test/java/com/example/pcroom/PcRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pcroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
